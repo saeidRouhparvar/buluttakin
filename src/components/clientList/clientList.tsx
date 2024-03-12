7eb06e3365c34f4f665679154e88832b524507b9
Saeid Rouhparvar <saeid.rohparvar@gmail.com>
2024-03-12 23:27:22 +0330

    create cleint list

---
+++ src/components/clientList/clientList.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from "react";
+import PanelBox from "../panelBox/panelBox";
+import style from "./clientList.module.css";
+
+interface Props {
+    avatar?:ReactNode
+    name?:string
+    due?:string
+    time?:string
+}
+
+const ClientList = ({avatar,name,due,time}:Props) => {
+  return (
+    <PanelBox>
+      <div className={style.main_content}>
+        <div className={style.avatar}>{avatar}</div>
+        <div className={style.title}>{name}</div>
+        <div className={style.title}>{due}</div>
+        <div className={style.title}>{time}</div>
+      </div>
+    </PanelBox>
+  );
+};
+
+export default ClientList;
