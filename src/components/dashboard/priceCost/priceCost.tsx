2c2870e69fddd1c8e60e4ba7c6fe73fe62aaf7f8
Saeid Rouhparvar <saeid.rohparvar@gmail.com>
2024-03-12 23:29:09 +0330

    create price cost

---
+++ src/components/dashboard/priceCost/priceCost.tsx
@@ -0,0 +1,28 @@
+import style from "../priceCost/priceCost.module.css";
+import { ReactNode } from "react";
+import PanelBox from "../../panelBox/panelBox";
+
+interface Props {
+  icon?: ReactNode;
+  price?: number;
+  title?: string;
+}
+
+const PriceCost = ({ icon, price, title }: Props) => {
+  return (
+    <div className={style.price_cost}>
+      <PanelBox>
+        <div className={style.main_content}>
+          <div className={style.icon}>{icon}</div>
+          <div className={style.prices}>
+            <div className={style.sign}>$</div>
+            <div className={style.price}>{price}</div>
+          </div>
+          <div className={style.title}>{title}</div>
+        </div>
+      </PanelBox>
+    </div>
+  );
+};
+
+export default PriceCost;
