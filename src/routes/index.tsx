51c5471681bdf23d43ce4ca337edc04f4236752a
Saeid Rouhparvar <saeid.rohparvar@gmail.com>
2024-03-12 23:31:36 +0330

    create route component

---
+++ src/routes/index.tsx
@@ -0,0 +1,87 @@
+import { createBrowserRouter } from "react-router-dom";
+import {
+  bank,
+  chartSquare,
+  email,
+  finance,
+  group,
+  homeHashtag,
+  list,
+  setting,
+} from "../components/Svg";
+import MainLayout from "../layouts/mainLayout";
+import Dashboard from "../components/dashboard/dashboard";
+import Redirect from "../components/redirect";
+
+const router = [
+  {
+    path: "/",
+    element: <Redirect navig="dashboard" />,
+  },
+  {
+    path: "/*",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "dashboard",
+        name: "Dashboard",
+        showInNav: true,
+        element: <Dashboard />,
+        icon: <div>{homeHashtag}</div>,
+      },
+      {
+        path: "report",
+        name: "Report",
+        showInNav: true,
+        element: <Dashboard />,
+        icon: <div>{chartSquare}</div>,
+      },
+      {
+        path: "bank",
+        name: "Bank",
+        showInNav: true,
+        element: <Dashboard />,
+        icon: <div>{bank}</div>,
+      },
+      {
+        path: "email",
+        name: "Email",
+        showInNav: true,
+        element: <Dashboard />,
+        icon: <div>{email}</div>,
+      },
+      {
+        path: "group",
+        name: "Group",
+        showInNav: true,
+        element: <Dashboard />,
+        icon: <div>{group}</div>,
+      },
+      {
+        path: "list",
+        name: "List",
+        showInNav: true,
+        element: <Dashboard />,
+        icon: <div>{list}</div>,
+      },
+      {
+        path: "finance",
+        name: "Finance",
+        showInNav: true,
+        element: <Dashboard />,
+        icon: <div>{finance}</div>,
+      },
+      {
+        path: "setting",
+        name: "Setting",
+        showInNav: false,
+        element: <Dashboard />,
+        icon: <div>{setting}</div>,
+      },
+    ],
+  },
+];
+
+const CreateRouter = createBrowserRouter(router);
+
+export { router, CreateRouter };
