fe922a91d02af5ce94aaba636055ca66e089e3a9
Saeid Rouhparvar <saeid.rohparvar@gmail.com>
2024-03-13 23:23:06 +0330

    create select box

--- src/components/selectBox/selextBox.tsx
+++ src/components/selectBox/selextBox.tsx
@@ -1,19 +1,51 @@
 import { arrowSelect } from "../Svg";
 import style from "./selectBox.module.css";
+import {ChangeEvent, ReactNode, useState} from "react";
 
-const SelectBox = () => {
+const SelectBox = ({placeholder}:{placeholder?:string,onApply:(data:unknown)=>void}) => {
+  const [active,setActive] = useState(false)
   return (
     <div className={style.container}>
       <div className={style.select}>
-        <div className={style.select_box}>
+        <div className={style.select_box}  onClick={()=>{
+          setActive(!active)
+        }}>
           <div className={style.select_title}>Employes involved</div>
           <div className={style.arrow_select}>{arrowSelect}</div>
-          {/* <div className={style.select_}>
-            <div className={style._box}>
-            <div className={style.search_box}><input/></div>
-            </div>
-          </div> */}
+
+          <div className={style.placeholder}>
+            {placeholder}
+          </div>
+
         </div>
+        {active &&
+        <div className={style.select_}>
+          <div className={style._box}>
+            <div className={style.search_box}>
+              <input placeholder={"Search Values"}/>
+            </div>
+            <form action="" onSubmit={(e:any)=>{
+              e.preventDefault()
+              Object.keys(e.target).map((item)=>{
+
+
+                console.log(e.target[item])
+              })
+            }}>
+              <div className="">
+                1
+                <input type={"checkbox"} name={"name[]"} value={"1"}/>
+              </div>
+
+              <div className="">
+                2
+                <input type={"checkbox"} name={"name[]"} value={"2"}/>
+              </div>
+              <input type="submit" value={"apply"}/>
+            </form>
+          </div>
+        </div>}
+
       </div>
     </div>
   );
