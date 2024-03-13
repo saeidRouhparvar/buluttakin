5826e82411e3743168f814d63962ae6e187ad64a
Saeid Rouhparvar <saeid.rohparvar@gmail.com>
2024-03-13 23:21:41 +0330

    create formation component

---
+++ src/components/rightSidePanel/formation/formation.tsx
@@ -0,0 +1,24 @@
+import Button from "../../button/button";
+import PanelBox from "../../panelBox/panelBox";
+import style from "./formation.module.css";
+
+const Formation = () => {
+  return (
+    <div className={style.container}>
+    <PanelBox>
+      <div className={style.main_content}>
+        <div className={style.formation_header}>Formation status</div>
+        <div className={style.formation_title}>In progress</div>
+        <div className={style.progress}>
+          <div className={style.bar}></div>
+        </div>
+        <div className={style.formation_title}>Estimated processing</div>
+        <div className={style.formation_title}>4-5 business days</div>
+        <div className={style.btn}></div>
+        <Button children="View status"/>
+      </div>
+    </PanelBox></div>
+  );
+};
+
+export default Formation;
