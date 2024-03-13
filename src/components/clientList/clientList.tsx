0ce1cfee8e1af5bc8bccc87872cd9c22ab6e8835
Saeid Rouhparvar <saeid.rohparvar@gmail.com>
2024-03-13 23:23:38 +0330

    add avatar to client list

--- src/components/clientList/clientList.tsx
+++ src/components/clientList/clientList.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import PanelBox from "../panelBox/panelBox";
 import style from "./clientList.module.css";
+import Avatar from "../avatar/avatar";
 
 interface Props {
     avatar?:ReactNode
@@ -13,7 +14,7 @@ const ClientList = ({avatar,name,due,time}:Props) => {
   return (
     <PanelBox>
       <div className={style.main_content}>
-        <div className={style.avatar}>{avatar}</div>
+        <div className={style.avatar}><Avatar src={`${avatar}`}/></div>
         <div className={style.title}>{name}</div>
         <div className={style.title}>{due}</div>
         <div className={style.title}>{time}</div>
