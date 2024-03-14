23d9493ceae7b7a590f9e236366320dce6fcdb4b
Saeid Rouhparvar <saeid.rohparvar@gmail.com>
2024-03-14 22:44:20 +0330

    add apply and cancel button to modal

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,15 +1,18 @@
 import { ReactNode } from "react";
 import { closeBtn } from "../Svg";
 import style from "./modal.module.css";
+import Button from "../button/button";
 
 interface Props {
   isOpen: boolean;
   onClose: boolean;
   closeClick:() =>void
   children?:ReactNode
+  acceptClick?:() => void
+  cancelClick?:() => void
 }
 
-const Modal = ({ isOpen, onClose ,closeClick,children}: Props) => {
+const Modal = ({ isOpen, onClose ,closeClick,acceptClick,cancelClick,children}: Props) => {
 
   return (
     <div className={onClose ? style.main_open : style.main_}>
@@ -19,6 +22,10 @@ const Modal = ({ isOpen, onClose ,closeClick,children}: Props) => {
       >
       <div className={style.close} onClick={closeClick}>{closeBtn}</div>
         {children}
+        <div className={style.modal_btn}>
+          <Button size="2.5rem" type="secoundry" children="Accept" onClick={acceptClick}/>
+          <Button size="2.5rem" type="primary" children="Cancel" onClick={cancelClick}/>
+        </div>
       </div>
     </div>
   );
