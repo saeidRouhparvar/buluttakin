aabf6e24bb29476ebfe657a0634f157c897f3d2f
Saeid Rouhparvar <saeid.rohparvar@gmail.com>
2024-03-14 22:40:47 +0330

    add height and width to avatar

--- src/components/avatar/avatar.tsx
+++ src/components/avatar/avatar.tsx
@@ -2,12 +2,14 @@ import style from './avatar.module.css'
 
 interface Props  {
     src?:string
+    w?:string
+    h?:string
 }
 
-const Avatar = ({src}:Props ) => {
+const Avatar = ({src,w,h}:Props ) => {
     return (
-        <div className={style.avatar} >
-            <img src={src} alt="" />
+        <div className={style.avatar} style={{width:w,height:h}}>
+            <img style={{width:w,height:h}} src={src} alt="" />
         </div>
 
     )
