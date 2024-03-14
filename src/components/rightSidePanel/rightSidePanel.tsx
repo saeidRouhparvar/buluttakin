fbfc7b21a35d2d1f58cebcd6c36de359ef860716
Saeid Rouhparvar <saeid.rohparvar@gmail.com>
2024-03-14 22:52:10 +0330

    add modal to right side

--- src/components/rightSidePanel/rightSidePanel.tsx
+++ src/components/rightSidePanel/rightSidePanel.tsx
@@ -13,10 +13,8 @@ import Formation from "./formation/formation";
 import style from "./rightSidePanel.module.css";
 import TodoList from "./totoList/todoList";
 import Input from "../input/input";
-
-// interface Props {
-//   onClick?:
-// }
+import SelectBox from "../selectBox/selextBox";
+import Chips from "../chips/chips";
 
 const RightSidePanel = () => {
   const [open, setOpen] = useState(false);
@@ -54,10 +52,33 @@ const RightSidePanel = () => {
         onClose={open}
         isOpen={open}
         closeClick={() => setOpen(false)}
+        cancelClick={() => setOpen(false)}
+        acceptClick={() => setOpen(false)}
         children={
           <div className={style.modal_content}>
             <div className={style.add_}>Add to do</div>
-            <Input isIcon/>
+            <Input isIcon />
+            <div className={style.select}>
+              <SelectBox
+                isAvatar
+               
+                listItem={[
+                  { id: 1, name: "Matin abbasi", src:"../../src/assets/icons/avatar_2.png"},
+                  { id: 2, name: "Matin ghaderi" },
+                  { id: 3, name: "Matin alipour" },
+                  { id: 4, name: "Reza jabari " },
+                  { id: 5, name: "Matin hasani" },
+                  { id: 6, name: "Darya safaee " },
+                  { id: 7, name: "Farhad mottagi " },
+                  { id: 8, name: "Matin mohamadi" },
+                  { id: 9, name: "Shahin noori " },
+                  { id: 10, name: "Matin kazemi" },
+                ]}
+                height="56px"
+              />
+            </div>
+
+            <Chips />
           </div>
         }
       />
