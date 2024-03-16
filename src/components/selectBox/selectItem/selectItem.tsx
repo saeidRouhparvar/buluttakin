578b7c109222e058b0891a590e468ef47e33871d
Saeid Rouhparvar <saeid.rohparvar@gmail.com>
2024-03-16 23:40:17 +0330

    refactor select item

--- src/components/selectBox/selectItem/selectItem.tsx
+++ src/components/selectBox/selectItem/selectItem.tsx
@@ -1,24 +1,57 @@
+import {useEffect, useState} from "react";
 import Avatar from "../../avatar/avatar";
 import style from "./selectItem.module.css";
 
 interface SelectProps {
-  value?: string;
-  onChange?:() => void;
-  name?: string;
-  listSrc?: string;
-  isAvatar?: boolean;
+    value?: string;
+    onChange?: (data: any) => void;
+    name?: string;
+    listSrc?: string;
+    isAvatar?: boolean;
+    active?: boolean;
 }
 
-const SelectItem = ({ value, listSrc, isAvatar, onChange, name }: SelectProps) => {
-  return (
-    <div className={style.main_content}>
-      <div className={style.select_line}>
-        {isAvatar && <Avatar src={listSrc} w="1.5rem" h="1.5rem"/>}
-        <input className={style.input} type={"checkbox"} value={value} onChange={onChange} />
-        {name}
-      </div>
-    </div>
-  );
+const SelectItem = ({
+                        value,
+                        listSrc,
+                        isAvatar,
+                        onChange,
+                        name,
+                        active
+                    }: SelectProps) => {
+
+    const [checked, setChecked] = useState(false);
+    useEffect(() => {
+        setChecked(active || false);
+    }, [active]);
+
+    return (
+        <div className={style.main_content} onClick={() => {
+            const e: any = {
+                target: {
+                    name: name
+                }
+            }
+            if (onChange) {
+                onChange(e)
+                setChecked(!checked)
+            }
+
+        }}>
+            <div className={!checked ? style.select_line : style.select_line_checked}>
+                {isAvatar && <Avatar src={listSrc} w="1.5rem" h="1.5rem"/>}
+                <input
+                    checked={checked}
+                    onClick={() => setChecked(!checked)}
+                    className={style.input}
+                    type={"checkbox"}
+                    name={value}
+
+                />
+                {name}
+            </div>
+        </div>
+    );
 };
 
 export default SelectItem;
