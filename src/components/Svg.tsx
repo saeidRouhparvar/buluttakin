f0ab7bf5cb40b5a2b9e1c98c77309490117704b7
Saeid Rouhparvar <saeid.rohparvar@gmail.com>
2024-03-16 23:35:02 +0330

    add new svg

--- src/components/Svg.tsx
+++ src/components/Svg.tsx
@@ -622,6 +622,15 @@ const searchIcon = <svg width="24" height="24" viewBox="0 0 24 24" fill="none" x
 <path fill-rule="evenodd" clip-rule="evenodd" d="M14.76 13.27L20.49 19L19 20.49L13.27 14.76C12.2 15.53 10.91 16 9.5 16C5.91 16 3 13.09 3 9.5C3 5.91 5.91 3 9.5 3C13.09 3 16 5.91 16 9.5C16 10.91 15.53 12.2 14.76 13.27ZM9.5 5C7.01 5 5 7.01 5 9.5C5 11.99 7.01 14 9.5 14C11.99 14 14 11.99 14 9.5C14 7.01 11.99 5 9.5 5Z" fill="#344054"/>
 </svg>
 
+const circlPurple = <svg width="9" height="10" viewBox="0 0 9 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+<ellipse cx="4.5" cy="4.5204" rx="4.5" ry="4.51064" fill="#6941C6"/>
+</svg>
+
+const circlBlack = <svg width="9" height="10" viewBox="0 0 9 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+<ellipse cx="4.5" cy="4.5204" rx="4.5" ry="4.51064" fill="#101828"/>
+</svg>
+
+
 export {
   homeHashtag,
   chartSquare,
@@ -647,5 +656,5 @@ export {
   receiptIcon,
   clockIcon,
   clipboardIcon,
-  addIcon,closeBtn,searchIcon
+  addIcon,closeBtn,searchIcon,circlBlack,circlPurple
 };
