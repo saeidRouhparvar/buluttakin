516de865565010062d52a553167164a814d3298d
Saeid Rouhparvar <saeid.rohparvar@gmail.com>
2024-03-16 23:39:20 +0330

    add type and refactor style in panel box

--- src/components/panelBox/panelBox.tsx
+++ src/components/panelBox/panelBox.tsx
@@ -3,10 +3,11 @@ import style from "./panelBox.module.css";
 
 interface Props {
   children?: ReactNode;
+  type?: 'border1' | 'border2'
 }
 
-const PanelBox = ({ children }: Props) => {
-  return <div className={style.panel}>{children}</div>;
+const PanelBox = ({ children,type }: Props) => {
+  return <div className={type === 'border1' ? style.panel_border1 : type === 'border2' ? style.panel_border2 : style.panel}>{children}</div>;
 };
 
 export default PanelBox;
